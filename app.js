0b7532b3b3c47fc94bae4e6258671e6806989e0a
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-05-16 10:06:08 -0500

    Add user and configure passport

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
 
-
+const User = require('./models/user');
 
 var app = express();
 
 
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require('express-session')({
+  secret: "This is a secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 // Connect to database
-// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://localhost/auth_demo_app");
 
 
 // Routes
