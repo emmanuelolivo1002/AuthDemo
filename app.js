5576ab483d7402bbf2c51352b553203408a14673
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-05-16 09:48:15 -0500

    Initial commits

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+
+var app = express();
+
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// Connect to database
+// mongoose.connect("mongodb://localhost/yelp_camp");
+
+
+// Routes
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+
+// Connect to server
+app.listen(process.env.PORT || 3000, process.env.IP, function() {
+  console.log("Listening to server");
+});
